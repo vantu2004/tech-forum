6117b0cbd3a886a2990b590c50f04e2f5cfe2b9c
vantu <tulevan526@gmail.com>
2025-09-26 13:40:29 +0700

    fix(resume): prevent crash when resume is null by using optional chaining

--- frontend/src/pages/resume_page/ResumePage.jsx
+++ frontend/src/pages/resume_page/ResumePage.jsx
@@ -16,7 +16,7 @@ const ResumePage = () => {
     <div className="container mx-auto max-w-7xl px-6 sm:px-6 lg:px-6 flex gap-6 mt-20 mb-6 text-black">
       <div className="flex-1">
         {/* Lấy CV đầu tiên từ mảng resume (TODO: thêm combobox chọn resume để load lên iframe, tạm thời load đỡ cái đầu tiên) */}
-        <ResumeContent cvUrl={userProfile?.resume[0]} />
+        <ResumeContent cvUrl={userProfile?.resume?.[0]} />
       </div>
       <div className="hidden lg:block w-1/4">
         <RightSidebar cvUrl={cvUrl} setCvUrl={setCvUrl} />
